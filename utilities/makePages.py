844a3f3718a346ed96fe94f1c33be165927f5d7d
helrond <helrond@hotmail.com>
2016-02-23 15:16:30 -0500

    adding social media meta tags

--- utilities/makePages.py
+++ utilities/makePages.py
@@ -15,6 +15,13 @@ def get_json(data):
         parsed_data = json.load(data_file)
     return parsed_data
 
+def get_note(note):
+    if note["jsonmodel_type"] == 'note_multipart':
+        content = note["subnotes"][0]["content"]
+    else:
+        content = note["content"]
+    return content
+
 def make_pages(src, dest):
     if os.path.exists(src):
         for f in os.listdir(src):
@@ -23,6 +30,20 @@ def make_pages(src, dest):
 
                 identifier = os.path.splitext(f)[0]
                 title = data["title"].strip().replace('"', "'")
+                raw_description = ''
+                description = ''
+
+                notes = data["notes"]
+                for note in notes:
+                    if note["type"] == 'abstract':
+                        raw_description = get_note(note)
+                    elif note["type"] == 'scopecontent':
+                        raw_description = get_note(note)
+                    elif note["type"] == 'bioghist':
+                        raw_description = get_note(note)
+                    else:
+                        pass
+                description = (raw_description.strip().replace('"', "'")[:200] + '...') if len(raw_description) > 200 else description
 
                 with open(os.path.join(dest,identifier+'.html'), 'w+') as new_file:
                     new_file.write("---\nlayout: "+dest+"\n")
@@ -30,6 +51,7 @@ def make_pages(src, dest):
                     new_file.write("id: "+identifier+"\n")
                     new_file.write("type: "+dest+"\n")
                     new_file.write("permalink: "+dest+"/"+identifier+"/\n")
+                    new_file.write("description: \""+description+"\"\n")
                     new_file.write("---")
                     new_file.close
                     # print str(os.path.join(dest,identifier+'.html')) + " created"
