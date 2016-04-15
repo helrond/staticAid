e75ead3d2076a1bf9c5ff82e3957c35453fbb8d0
J. Luke Scott <luke@cronworks.net>
2016-04-15 11:58:48 -0700

    converted config 'Build' section to use a less-awkward 'dest: src' format instead of 'key: src,dest'

--- utilities/makePages.py
+++ utilities/makePages.py
@@ -8,16 +8,8 @@ configFilePath = os.path.join(current_dir, 'local_settings.cfg')
 config = ConfigParser.ConfigParser()
 config.read(configFilePath)
 
-destinations = [config.get('Build', 'collections'), 
-                config.get('Build', 'families'), 
-                config.get('Build', 'organizations'), 
-                config.get('Build', 'people'), 
-                config.get('Build', 'software'), 
-                config.get('Build', 'objects'),
-                ]
-
 def get_json(data):
-    with open(os.path.join(src,data)) as data_file:
+    with open(os.path.join(src, data)) as data_file:
         parsed_data = json.load(data_file)
     return parsed_data
 
@@ -57,20 +49,16 @@ def make_pages(src, dest):
                         pass
                 description = (raw_description.strip().replace('"', "'")[:200] + '...') if len(raw_description) > 200 else description
 
-                with open(os.path.join(dest,identifier+'.html'), 'w+') as new_file:
-                    new_file.write("---\nlayout: "+dest+"\n")
-                    new_file.write("title: \""+title.encode('utf-8')+"\"\n")
-                    new_file.write("id: "+identifier+"\n")
-                    new_file.write("type: "+dest+"\n")
-                    new_file.write("permalink: "+dest+"/"+identifier+"/\n")
-                    new_file.write("description: \""+description.encode('utf-8')+"\"\n")
+                with open(os.path.join(dest, identifier + '.html'), 'w+') as new_file:
+                    new_file.write("---\nlayout: " + dest + "\n")
+                    new_file.write("title: \"" + title.encode('utf-8') + "\"\n")
+                    new_file.write("id: " + identifier + "\n")
+                    new_file.write("type: " + dest + "\n")
+                    new_file.write("permalink: " + dest + "/" + identifier + "/\n")
+                    new_file.write("description: \"" + description.encode('utf-8') + "\"\n")
                     new_file.write("---")
                     new_file.close
                     # print str(os.path.join(dest,identifier+'.html')) + " created"
 
-for destination in destinations:
-    split = destination.split(',')
-    src = split[0]
-    dest = split[1]
+for dest, src in config.items('Build'):
     make_pages(src, dest)
-
