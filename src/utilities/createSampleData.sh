876572263992e8c0cb84029e0b8ad20a4e20d5bb
Luke Scott <v-lukes@gatesarchive.com>
2016-04-21 13:20:58 -0700

    fixed bad directory structure in data-cleaned.zip; createSampleData.sh accepts .zip filename parameter

--- src/utilities/createSampleData.sh
+++ src/utilities/createSampleData.sh
@@ -3,13 +3,21 @@
 # TODO convert this into a pluggable/configurable backend module
 # when the backend refactor is done.
 
-DIR=$(cd $(dirname $(readlink -f "$0" ))/../..; pwd)
+SRC=$1
 
-DATA=$DIR/build/data
-SRC=$DIR/data/data-cleaned.zip
+ROOT=$(cd $(dirname $(readlink -f "$0" ))/../..; pwd)
+DATA=$ROOT/build/data
+
+if [ "x$SRC" = "x" ]
+then
+	echo "Please tell me what .zip file you'd like to extract into $DATA by using the format:"
+	echo ""
+	echo "    $0 </path/to/data.zip>"
+	echo ""
+	exit 1
+fi
 
 echo "Extracting $SRC to $DATA..."
 
 mkdir -p $DATA
 unzip $SRC -d $DATA/
-
