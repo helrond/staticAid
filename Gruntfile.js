520630b24f8ccf85917d107e287c6c5b07ebbf55
J. Luke Scott <luke@cronworks.net>
2016-04-20 20:41:52 -0700

    moved _site to build/; updated .gitignore so that dirs are rooted

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,15 +25,17 @@ module.exports = function(grunt) {
     jekyll: {
       serve: {
           options: {
-          serve: true,
-          skip_initial_build: true,
-          verbose: true,
-          incremental: true
+            serve: true,
+            dest: 'build/_site/',
+            skip_initial_build: true,
+            verbose: true,
+            incremental: true
         }
       },
       build: {
         options: {
           serve: false,
+          dest: 'build/_site/',
           verbose: true
         }
       }
