31d58068d6741ab74aeb73ca597f22a05f850a74
Hillel Arnold <helrond@hotmail.com>
2021-05-30 16:38:55 -0400

    adds update bootstrap

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ then
 
   echo "Site available at http://localhost:4000"
 
-  inotifywait -e modify,move,create,delete -m site/ static_aid/ --exclude __pycache__* cassettes -r |
+  inotifywait -e modify,move,create,delete -m site/ static_aid/ --exclude __pycache__* static_aid/tests/cassettes -r |
   while read filename; do
     if [[ "$filename" == site/* ]]; then
       echo "Regenerating..."
